5535c2337574d24e52d2678671ed21326acedf60
Svetlana-Ruban86 <galasvetlana2020@gmail.com>
2025-03-26 00:51:22 +0300

    Откорректировала изменения

--- assets/index.js
+++ assets/index.js
@@ -1,14 +1,12 @@
 function addTask(){
     const taskInput=document.getElementById(`task-input`);
     const taskText=taskInput.value;
-   
     const taskList=document.getElementById(`task-list`);
 const newTask=document.createElement(`li`);
 newTask.textContent=taskText;
 newTask.addEventListener(`click`,function(){
-    newTask.classList.toggle(`completed`)
-});
-
+    newTask.taskList.toggle(`completed`)
+})
 taskList.append(newTask);
 taskInput.value="";
 }
